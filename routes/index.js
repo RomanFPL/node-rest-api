d9aa3a4b1d19ca14a8fdbdf18d038dade7120288
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-14 05:09:46 +0300

    Add storege & folders

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const notes = require('../repositories')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/notes', function(req, res, next) {
+  res.json({notes});
 });
 
 module.exports = router;
